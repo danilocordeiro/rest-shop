f142192218f1bf3e54a92a5dafe369eefd7f8abd
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2018-06-04 13:17:24 -0300

    Add signup and login

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express  = require('express');
+const router   = express.Router();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt   = require('bcrypt');
+const jwt      = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 
@@ -43,6 +45,49 @@ router.post('/signup', (req, res, next) => {
         }
       });
     }
+  });
+});
+
+router.post('/login', (req, res, next) => {
+  User.find({email: req.body.email})
+  .exec()
+  .then(user => {
+    if(user.length < 1) {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    }
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if(err) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      if(result) {
+        const token = jwt.sign({
+          email: user[0].email,
+          userId: user[0].userId
+        }, 
+        process.env.JWT_KEY,
+        {
+          expiresIn: "1h"
+        }
+      );
+        return res.status(200).json({
+          message: 'Auth successfully',
+          token: token
+        });
+      }
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
   });
 });
 
